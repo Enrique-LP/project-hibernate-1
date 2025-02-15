91445bd0082bdc738fa8c208006cfb729f363e0c
Enrique-LP <jesus902009@live.com>
2025-02-15 12:00:07 -0600

    refactor: modernizar configuración de Spring MVC
    - Reemplazar WebMvcConfigurerAdapter por WebMvcConfigurer
    - Implementar métodos requeridos (getMessageCodesResolver, getValidator)
    - Actualizar configuración de recursos y vistas
    - Mejorar configuración de conversores de mensajes

--- src/main/java/com/game/config/WebConfig.java
+++ src/main/java/com/game/config/WebConfig.java
@@ -2,9 +2,6 @@ package com.game.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,34 +11,18 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.game")
-public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
-
-    private ApplicationContext applicationContext;
-
-    public void setApplicationContext(@Autowired ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
-
-    @Bean
-    public ThymeleafViewResolver viewResolver() {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine());
-        return viewResolver;
-    }
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("my");
+        registry.addViewController("/").setViewName("forward:/html/my.html");
     }
 
     @Override
@@ -52,22 +33,11 @@ public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
     }
 
     @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("/html/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCacheable(false);
-        return templateResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        templateEngine.setEnableSpringELCompiler(false);
-        return templateEngine;
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/");
+        resolver.setSuffix(".html");
+        return resolver;
     }
 
     @Override
@@ -76,5 +46,15 @@ public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
         converter.getObjectMapper().setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
         converter.getObjectMapper().setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
         converters.add(converter);
+    }
+
+    @Override
+    public org.springframework.validation.MessageCodesResolver getMessageCodesResolver() {
+        return null;
+    }
+
+    @Override
+    public org.springframework.validation.Validator getValidator() {
+        return null;
     }
 }
